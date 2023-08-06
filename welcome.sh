4ff6a485c88edce0c197903599ea6cd74cfda9c1
MD Mahfuz Reham <78415545+mahfuzreham@users.noreply.github.com>
2023-08-06 20:42:17 +0600

    Add files via upload

---
+++ welcome.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+echo "  ####################### Personal Information Entry #######################  "
+
+read -p "Name   :-" name
+read -p "Age :-" age
+read -p "Mobile Number :-" number
+read -p "E-mail :-" $mail
+echo "Final Information For Print"
+
+echo "Name=$name"
+echo "Age=$age"
+echo "Mobile=$number"
+echo "E-mail="$mail"
+
+echo "Thanks Md Mahfuz Reham"
+
